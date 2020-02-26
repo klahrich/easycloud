f2b8f28d10f18ee8e4f4093df62b41b3237bc473
Karim Lahrichi <klahrichi@flinks.io>
2020-02-26 11:15:13 -0500

    Refactored folder structure

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Karim Lahrichi',
     author_email='klahrich@gmail.com',
     packages=find_packages(),
-    install_requires=['pandas', 'google-cloud-bigquery', 'google-cloud-bigquery-storage', 'apache-beam[gcp]'],
+    install_requires=['pandas', 'google-cloud-bigquery', 'google-cloud-bigquery-storage', 'apache-beam[gcp]', 'pyaml'],
     version='0.1',
     license='MIT',
     description='A collection of easy to remember functions for manipulating data on cloud platforms',
