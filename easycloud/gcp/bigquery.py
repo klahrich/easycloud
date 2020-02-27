1cd882f65128f86247c9c061623c6ed33c304fb2
Karim Lahrichi <karimlahrichi@hotmail.com>
2020-02-27 18:43:24 -0500

    Update bigquery.py

--- easycloud/gcp/bigquery.py
+++ easycloud/gcp/bigquery.py
@@ -207,36 +207,3 @@ class Bigquery:
             df = self.list_rows(dataset, table)
             df.to_csv(filepath, index=False)
             return df
-
-
-class Dataflow:
-
-    def __init__(self, job_name, project, temp_location, input_table, output_table, output_schema, extra_packages=None):
-        self.options = {}
-        self.options['job_name'] = job_name
-        self.options['project'] = project
-        self.options['temp_location'] = temp_location
-        self.options['staging_location'] = temp_location
-        self.options['runner'] = 'DataflowRunner'
-        self.options['setup_file'] = './setup.py'
-        self.options['extra_packages'] = [extra_packages]
-        self.input_table = project + ':' + input_table
-        self.output_table = project + ':' + output_table
-        self.output_schema = output_schema
-
-    def run(self, runf_func):
-        logging.getLogger().setLevel(logging.INFO)
-
-        pipeline_options = PipelineOptions.from_dictionary(self.options)
-        pipeline_options.view_as(SetupOptions).save_main_session = True
-
-        with beam.Pipeline(options=pipeline_options) as p:
-            p = (p | 'read_bq_table' >> beam.io.Read(beam.io.BigQuerySource(self.input_table)))
-            p = runf_func(p)
-            (p | 'write_bq_table' >> beam.io.gcp.bigquery.WriteToBigQuery(
-                                        self.output_table,
-                                        schema = self.output_schema,
-                                        create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
-                                        write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE))
-
-
