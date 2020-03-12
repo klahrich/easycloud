6bf8c6e30f10fd5c1d8bd593c00b23af3f7dca7b
Karim Lahrichi <klahrichi@flinks.io>
2020-03-12 11:23:30 -0400

    Refactored dataflow into gcp.py

--- easycloud/gcp.py
+++ easycloud/gcp.py
@@ -14,7 +14,13 @@ import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.options.pipeline_options import SetupOptions
 from pathlib import Path
-
+import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.options.pipeline_options import SetupOptions
+import yaml
+import importlib
+import argparse
+import logging
 
 class Bigquery:
     '''
@@ -272,3 +278,54 @@ class Bucket:
 	def upload_file(self, filepath: str):
 		p = Path(filepath)
 		self.bucket.blob(p.name).upload_from_filename(filepath)
+
+
+class Dataflow:
+
+    def __init__(self, job_name, project, temp_location, input_table, output_table, output_schema, setup_file):
+        self.options = {}
+        self.options['job_name'] = job_name
+        self.options['project'] = project
+        self.options['temp_location'] = temp_location
+        self.options['staging_location'] = temp_location
+        self.options['runner'] = 'DataflowRunner'
+        self.options['setup_file'] = setup_file
+        self.input_table = project + ':' + input_table
+        self.output_table = project + ':' + output_table
+        self.output_schema = output_schema
+
+    def run(self, runf_func):
+        logging.getLogger().setLevel(logging.INFO)
+
+        pipeline_options = PipelineOptions.from_dictionary(self.options)
+        pipeline_options.view_as(SetupOptions).save_main_session = True
+
+        with beam.Pipeline(options=pipeline_options) as p:
+            p = (p | 'read_bq_table' >> beam.io.Read(beam.io.BigQuerySource(self.input_table)))
+            p = runf_func(p)
+            (p | 'write_bq_table' >> beam.io.gcp.bigquery.WriteToBigQuery(
+                                        self.output_table,
+                                        schema = self.output_schema,
+                                        create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
+                                        write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE))
+
+
+# TODO: add a dataflow command to make it more obvious what we're doing
+if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('runner')
+	parser.add_argument('--config_file')
+
+	args = parser.parse_args()
+
+	with open(args.config_file) as f:
+		config = yaml.load(f, Loader=yaml.FullLoader)
+
+	runner_module = importlib.import_module(args.runner)
+
+	flow = Dataflow(**config)
+	flow.run(runner_module.run)
+
+
+
